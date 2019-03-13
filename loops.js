a10c4b16027f1c1fb58f2001fc0bdb43ac97431a
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 03:42:04 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array)
+{
+  for(let i = 0; i < 25; i++)
+  {
+    if(i === 1)
+    array.push("I am 1 strange loop.");
+    else
+    array.push(`I am ${i} strange loops.`);
+  }
+  
+  return array;
+}
+
+function whileLoop(n)
+{
+    var num = n;
+    while(num > 0){
+      console.log(num);
+      if(num === 0)
+        console.log("done");
+    }
+}
